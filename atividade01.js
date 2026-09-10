df86d09e68babe966c99ef40bf431146a0370b32
ARTHUR SCHWARTZ GAULKE <arthur_gaulke@estudante.sesisenai.org.br>
2026-09-09 22:13:02 -0300

    aulas_javascript

--- atividade01.js
+++ atividade01.js
@@ -1,55 +1,108 @@
 // Prototipo cadastro itens do Roblox
 // Alunos: João e Arthur
+const readline = require("readline");
 
+const r1 = readline.createInterface({
+    input: process.stadin
+    output: process.stdout
+});
+
+funtion perguntar (texto) {
+    return new Promise(resolve => {
+        r1.question(texto, resposta 
+=> {
+            resolve(resposta);
+        })
+    });
+}
+
+//===========================================================================================
+// CATÁLOGO DE ITENS
+// ===========================================================================================
+
+const catalogo = [
+    {
+        nome: "Espada das Sombras",
+        preco: 250,
+        raridade: "Raro"
+        estoque: 12
+    },
+    {
+        nome: "Passe Bluester"
+        preco: 500
+        raridade: "Lendario"
+        estoque: 5
+    },
+    {
+        nome: "Skin Harpia"
+        preco: 600,
+        raridade: "Raro"
+        estoque: 10
+    },
+    {
+        nome: "pet lanterna"
+        preco: 100
+        raridade: "Comun"
+        estoque: 20
+
+
+    }
+];
+
+asyn function programa() {
+
+// ========================================================================================
+// ESCOLHA DO ITEM
+// ========================================================================================
+
+    console.log("=== ESCOLHA UM ITEM===");
+    catalogo.forEach((item, index) =>
+{
+        console.log(`${index + 1} - $ {item.nome}`);
+});
+
+        const itemEscolhido
+    catalogo[escolha - 1];
+
+}
 // ===================================================================
 // ETAPA 1 - FICHA DOS ITENS (VARIAVEIS, TIPOS E TEMPLATE STINGS)
 //====================================================================
 
-const nomeItem = "Capa do Lapislazule";
-const precoItem = 300; 
-let raridadeItem = "Raro"
-let quantidadeEstoque = 12;
-
-console.log; (`=== FICHA DO ITEM ===
-Nome: ${nomeItem}
-Preço: R$ ${precoItem}
-Raridade: ${raridadeItem}
-Estoque: ${quantidadeEstoque} unidades
+        console.log(`=== FICHA DO ITEM ===
+Nome: ${itemEscolhida.nome};
+preço: R$ ${itemEscolhida.preco} 
+Raridade: ${itemEscolhida.raridade}
+Estoques: ${itemEscolhida.estoque}
+unidades
 `);
 
 // =====================================================================
 // ETAPA 2 - REGRAS DE NEGÓCIO (OPERADORES E CONDICIONAIS)
 // =====================================================================
 
-if (precoItem >= 0) {
-    console.log("Preço válido. ");
-}   else {
-    console.log("Erro: o preço não pode ser negativo!");
+console.log("=== REGRAS DE NEGÓCIO ===")
+
+if (itemEscolhida.preco >= 0) {
+    console.log("Preço inválidade.");
 }
 
-if (precoItem < 100) {
-    raridadeItem = "Comun";
-}    else if (precoItem >= 100 && precoItem < 500) {
-    raridadeItem = "Lendario";
-}
-console.log (`Raridade classificada automatica: ${raridadeItem}`)
-
-const emDestaque = precoItem > 500 ? true : false;
-console.log(`Item em destaque? ${emDestaque}`);
-
-const disponivelParacompra = quantidadeEstoque > 0 && precoItem > 0;
-console.log(`Disponivel para compra? ${disponivelParacompra}\n`);
-
+if (itemEscolhido.preco < 100) {
+    itemEscolhido.raridade = "Comun";
+} else if (itemEscolhido.preco < 500) {
+      itemEscolhida.raridade = "Raro";                                        
+} else {
+    
 // ===============================================================================
 // ETAPA 3 - CADASTRO EM LOTE (LAÇO FOR)
 // ===============================================================================
 
 console.log("=== CADASTRO EM LOTE ===");
-for (let i = o; i < 5; i++) {
+for (let i = 0; i < 5; i++) {
     console.log(`Item ${i + 1} cadastro com sucesso`)
     
     if (i % 2 === 0) {
-        console.log("Item em promoção dsa semana!");
+        console.log("Item em promoção dessa semana!");
     }
 }
 console.log("");
@@ -59,8 +112,8 @@ console.log("");
 // =================================================================================
 
 console.log("=== CONTROLE DE ESTOQUE ===");
-while (quantidade > 0) {
-    quantidadeEstoque_= 2;
+while (quantidadeEstoque > 0) {
+    quantidadeEstoque-= 2;
     console.log(`Estoque resstante: ${quantidadeEstoque}`);
 }
 console.log("Item esgotado!");
@@ -71,13 +124,14 @@ console.log("");
 // =====================================================================================
 
 console.log("=== CATÁLOGO DE ITENS (VITRINE) ===")
-const catalago = [
+const catalogo = [
     "Capa do Lapislazule",
     "Passe Bluestar",
     "Pet lanterna", 
     "Skin Harpia"
+
 ];
 
 for (const item of catalogo) {
-    console.log(` \("-")/ ${item}`);
+    console.log(` ${item} `);
 }
